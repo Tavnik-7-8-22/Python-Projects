20d2479615620ccb1fac40f3d49f510ae3aa2910
tavnik-7-8-22 <fireygamer575@gmail.com>
2023-04-23 02:39:39 -0400

    Finished created the "LocationMoveWithGrid" file now movement is functional waiting on further functionality

--- pythonProject/LocationMoveWithGrid.py
+++ pythonProject/LocationMoveWithGrid.py
@@ -11,17 +11,16 @@ class Location:
         self.tree = "[" + Colors.GREEN2 + "T" + Colors.END + "]"
         self.barrier = "#"
         # self.start = [1, 1]
-        self.curr_pos = [1, 1]
+        # self.curr_pos = [1, 1]
         # self.prev_pos = self.start
         self.grid = []
         self.location_data = ""
         self.move_count = 5
         self.visibility = 3
+        self.update_temp_var = "[ ]"
 
     def set_location(self):
-        for i in range(len(self.grid)):
-            for j in range(len((self.grid[i]))):
-                self.curr_pos = [int(len(loc.grid)/2), int(len(loc.grid)/2)]
+        self.grid[len(loc.grid)//2][len(loc.grid)//2] = self.player
 
     def get_data(self):
         f = open('JSON files/Location_info.json')
@@ -79,7 +78,6 @@ class Location:
                 elif random_gen <= 100:
                     random_gen = self.empty_space
                 self.grid[i][j] = random_gen
-        self.grid[self.curr_pos[0]][self.curr_pos[1]] = self.player
 
     def randomize_grid_north(self):
         for i in range(len(self.grid)):
@@ -94,7 +92,7 @@ class Location:
                         random_gen = self.empty_space
                     self.grid[i][j] = random_gen
 
-    def randomize_grid_east(self):
+    def randomize_grid_west(self):
         for i in range(len(self.grid)):
             for j in range(len(self.grid[i])):
                 if j == 0:
@@ -120,7 +118,7 @@ class Location:
                         random_gen = self.empty_space
                     self.grid[i][j] = random_gen
 
-    def randomize_grid_west(self):
+    def randomize_grid_east(self):
         for i in range(len(self.grid)):
             for j in range(len(self.grid[i])):
                 if j == len(self.grid) - 1:
@@ -136,13 +134,71 @@ class Location:
     def update_grid_north(self):
         for i in reversed(range(len(self.grid))):
             for j in reversed(range(len(self.grid[i]))):
+                # print(temp_var)
                 if i != 0:
-                    # print(self.grid[i][j])
-                    # print(self.grid[i-1][j])
                     if self.grid[i-1][j] == self.player:
-                        self.grid[i][j] = self.empty_space
+                        self.grid[(len(loc.grid)//2)+1][len(loc.grid)//2] = self.update_temp_var
+                        print(self.update_temp_var)
+                        print("Changed to temp var")
+                    elif self.grid[i][j] == self.player:
+                        self.update_temp_var = self.grid[(len(loc.grid) // 2) - 1][len(loc.grid) // 2]
+                        print(self.update_temp_var)
+                        print("Assigned temp var")
                     else:
-                        self.grid[i][j] = self.grid[i - 1][j]
+                        self.grid[i][j] = self.grid[i-1][j]
+
+    def update_grid_west(self):
+        for i in reversed(range(len(self.grid))):
+            for j in reversed(range(len(self.grid[i]))):
+                # print(temp_var)
+                if j != 0:
+                    if self.grid[i][j-1] == self.player:
+                        self.grid[len(loc.grid)//2][(len(loc.grid)//2)+1] = self.update_temp_var
+                        print(self.update_temp_var)
+                        print("Changed to temp var")
+                    elif self.grid[i][j] == self.player:
+                        self.update_temp_var = self.grid[(len(loc.grid))//2][(len(loc.grid)//2)-1]
+                        print(self.update_temp_var)
+                        print("Assigned temp var")
+                    else:
+                        self.grid[i][j] = self.grid[i][j-1]
+
+    def update_grid_south(self):
+        # print(f"{(len(loc.grid)//2)+1,(len(loc.grid))//2}")
+        # print(f"{self.grid[(len(loc.grid)//2)+1][(len(loc.grid))//2]}")
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[i])):
+                # print(f"{i, j}")
+                if i != len(self.grid)-1:
+                    if self.grid[i+1][j] == self.player:
+                        self.grid[(len(loc.grid)//2)-1][len(loc.grid)//2] = self.update_temp_var
+                        print(self.update_temp_var)
+                        print("Changed to temp var")
+                    elif self.grid[i][j] == self.player:
+                        self.update_temp_var = self.grid[(len(loc.grid)//2)+1][(len(loc.grid))//2]
+                        print(self.update_temp_var)
+                        print("Assigned temp var")
+                    else:
+                        self.grid[i][j] = self.grid[i+1][j]
+
+    def update_grid_east(self):
+        # print(f"{(len(loc.grid)//2)+1,(len(loc.grid))//2}")
+        # print(f"{self.grid[(len(loc.grid)//2)+1][(len(loc.grid))//2]}")
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[i])):
+                # print(f"{i, j}")
+                # print(temp_var)
+                if j != len(self.grid)-1:
+                    if self.grid[i][j+1] == self.player:
+                        self.grid[len(loc.grid)//2][(len(loc.grid)//2)-1] = self.update_temp_var
+                        print(self.update_temp_var)
+                        print("Changed to temp var")
+                    elif self.grid[i][j] == self.player:
+                        self.update_temp_var = self.grid[len(loc.grid)//2][((len(loc.grid))//2)+1]
+                        print(self.update_temp_var)
+                        print("Assigned temp var")
+                    else:
+                        self.grid[i][j] = self.grid[i][j+1]
 
     def player_movement(self):
         if self.move_count > 5:
@@ -165,69 +221,100 @@ class Location:
                     "west", "north east", "south east", "south west", "n", "ne", "e", "se", "s", "sw", "w", "nw"]:
             if move in ["west", "north-west", "north", "north-east", "east", "south-east", "south", "south-west"]:
                 if move == "west":
+                    self.update_grid_west()
                     self.randomize_grid_west()
                 elif move == "north-west":
+                    self.update_grid_north()
+                    self.update_grid_west()
                     self.randomize_grid_north()
                     self.randomize_grid_west()
                 elif move == "north":
                     self.update_grid_north()
                     self.randomize_grid_north()
                 elif move == "north-east":
+                    self.update_grid_north()
+                    self.update_grid_east()
                     self.randomize_grid_north()
                     self.randomize_grid_east()
                 elif move == "east":
+                    self.update_grid_east()
                     self.randomize_grid_east()
                 elif move == "south-east":
+                    self.update_grid_south()
+                    self.update_grid_east()
                     self.randomize_grid_south()
                     self.randomize_grid_east()
                 elif move == "south":
+                    self.update_grid_south()
                     self.randomize_grid_south()
                 elif move == "south-west":
+                    self.update_grid_south()
+                    self.update_grid_west()
                     self.randomize_grid_south()
                     self.randomize_grid_west()
             elif move in ["north west", "north east", "south east", "south west"]:
                 if move == "north west":
+                    self.update_grid_north()
+                    self.update_grid_west()
                     self.randomize_grid_north()
                     self.randomize_grid_west()
                 elif move == "north east":
+                    self.update_grid_north()
+                    self.update_grid_east()
                     self.randomize_grid_north()
                     self.randomize_grid_east()
                 elif move == "south east":
+                    self.update_grid_south()
+                    self.update_grid_east()
                     self.randomize_grid_south()
                     self.randomize_grid_east()
                 elif move == "south west":
+                    self.update_grid_south()
+                    self.update_grid_west()
                     self.randomize_grid_south()
                     self.randomize_grid_west()
             elif move in ["n", "ne", "e", "se", "s", "sw", "w", "nw"]:
                 if move == "w":
+                    self.update_grid_west()
                     self.randomize_grid_west()
                 elif move == "nw":
+                    self.update_grid_north()
+                    self.update_grid_west()
                     self.randomize_grid_north()
                     self.randomize_grid_west()
                 elif move == "n":
+                    self.update_grid_north()
                     self.randomize_grid_north()
                 elif move == "ne":
+                    self.update_grid_north()
+                    self.update_grid_east()
                     self.randomize_grid_north()
                     self.randomize_grid_east()
                 elif move == "e":
+                    self.update_grid_east()
                     self.randomize_grid_east()
                 elif move == "se":
+                    self.update_grid_south()
+                    self.update_grid_east()
                     self.randomize_grid_south()
                     self.randomize_grid_east()
                 elif move == "s":
+                    self.update_grid_south()
                     self.randomize_grid_south()
                 elif move == "sw":
+                    self.update_grid_south()
+                    self.update_grid_west()
                     self.randomize_grid_south()
                     self.randomize_grid_west()
         else:
             if move == "dev-7-8-22":
                 while True:
-                    devtrig = input("You are now in dev testing mode, what do you want to run: ")
-                    if devtrig == "end":
+                    dev_funct = input("You are now in dev testing mode, what do you want to run: ")
+                    if dev_funct == "end":
                         break
             else:
                 print("Please input a valid option.")
-        self.grid[self.curr_pos[0]][self.curr_pos[1]] = self.player
+        self.set_location()
         # if 0 <= self.curr_pos[0] < len(self.grid) and 0 < self.curr_pos[1] <= len(self.grid):
         #     self.curr_pos = previous
         # else:
@@ -238,8 +325,8 @@ class Location:
 loc = Location()
 loc.get_data()
 loc.create_location_grid(3, 3)
-loc.set_location()
 loc.randomize_grid()
+loc.set_location()
 loc.print_location_grid(loc.grid)
 while True:
     loc.player_movement()
